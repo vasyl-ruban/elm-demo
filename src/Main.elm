ee26ce90c315101c83d136770e184cab1d349a86
v-ruban <v.ru6an@gmail.com>
2017-03-14 23:43:49 +0200

    elm architecture

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,42 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (..)
 
 main =
-  div [] [text "hello world"]
+  Html.beginnerProgram {
+    model = initMode "initValue" 0,
+    view = view,
+    update = update
+  }
+
+-- Model
+
+type alias Model =
+  { foo: String
+  , bar: Int
+  }
+
+initMode : String -> Int -> Model
+initMode foo bar =
+  Model foo bar
+
+-- Messages
+
+type Msg
+  = Click
+
+-- Update
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Click -> { model | foo = "afterClick" }
+
+-- View
+view : Model -> Html Msg
+view model =
+  div []
+  [text model.foo
+  , button [ onClick Click] [text "btn"]
+  ]
