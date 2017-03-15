be8ed424ee6be3ab9df026f6f6adeffe2fc37506
Vasyl.Ruban <Vasyl.Ruban@evry.com>
2017-03-15 14:38:05 +0200

    added input for querrying exact person

--- src/Inc.elm
+++ src/Inc.elm
@@ -1,25 +1,36 @@
 module Inc exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import Json.Decode as Decode
 
+type alias Person =
+  { name: String
+  , height: String
+  , mass: String
+  }
+
+initPerson: Person
+initPerson =
+  Person "" "" ""
 
 type alias Model =
   { val: Int
-  , name: String
+  , person: Person
   }
 
 initModel: Model
 initModel =
-  Model 0 "foo"
+  Model 0 initPerson
 
 type Msg
   = Inc
   | Dec
+  | ChangeVal String
   | LoadData
-  | HandleData (Result Http.Error String)
+  | HandleData (Result Http.Error Person)
 
 update: Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -29,30 +40,43 @@ update msg model =
     Dec ->
       ({model | val = model.val - 1}) ! []
 
-    LoadData ->
-      (model, getData)
+    ChangeVal newVal ->
+      case String.toInt newVal of
+        Ok val ->
+          ({model | val = val}, Cmd.none)
+        Err val ->
+          (model, Cmd.none)
 
-    HandleData (Ok name) ->
-      (Model 0 name, Cmd.none)
+    LoadData ->
+      (model, getData model)
+
+    HandleData (Ok person) ->
+      (Model model.val person, Cmd.none)
 
     HandleData (Err _) ->
-      (Model 0 "", Cmd.none)
+      (Model 0 initPerson, Cmd.none)
 
-getData: Cmd Msg
-getData =
+getData: Model -> Cmd Msg
+getData model =
   let
-    url = "http://swapi.co/api/people/1/"
+    url = "http://swapi.co/api/people/" ++ toString model.val
   in
     Http.send HandleData (Http.get url decoder)
 
-decoder: Decode.Decoder String
+decoder: Decode.Decoder Person
 decoder =
-  Decode.at ["name"] Decode.string
+  Decode.map3 Person
+    (Decode.field "name" Decode.string)
+    (Decode.field "mass" Decode.string)
+    (Decode.field "height" Decode.string)
+
 
 view : Model -> Html Msg
 view model =
   div []
     [text (toString model)
+    , br [] []
+    , input [ placeholder "person id", value (toString model.val), onInput ChangeVal ] []
     , button [onClick Inc] [text "inc"]
     , button [onClick Dec] [text "dec"]
     , button [onClick LoadData] [text "load"]
